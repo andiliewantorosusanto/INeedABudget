12d093a3cf0bfa9179abfefb802740e2d7a50c80
andiliewantorosusanto <andiliesusanto@gmail.com>
2022-03-27 16:17:42 +0700

    Test Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="relative min-h-screen md:flex">
+
+      <div className='bg-blue-800 text-blue-100 flex justify-between md:hidden'>
+        <a href="www.google.com" className='block p-4 text-white font-bold'>
+          Better Dev
         </a>
-      </header>
+
+        <button className='mobile-menu-button p-4 focus:outline-none focus:bg-blue-700'>
+          <svg className='h-5 w-5' 
+            xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
+
+      <div className="sidebar bg-blue-800 text-blue-100 w-64 space-y-6 px-2 py-7 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
+        <a href="www.google.com" className="text-white flex items-center space-x-2 px-4">
+          <svg className="w-8 h-8"
+            xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+          </svg>
+          <span className="text-2xl font-extrabold">
+            Better Dev
+          </span>
+        </a>
+
+        <nav>
+          <a href="www.google.com" className="block py-2.5 px-4 rounded hover:bg-blue-700">
+            Home
+          </a>
+          <a href="www.google.com" className="block py-2.5 px-4 rounded hover:bg-blue-700">
+            About
+          </a>
+          <a href="www.google.com" className="block py-2.5 px-4 rounded hover:bg-blue-700">
+            Features
+          </a>
+          <a href="www.google.com" className="block py-2.5 px-4 rounded hover:bg-blue-700">
+            Pricing
+          </a>
+        </nav>
+      </div>
+      <div className="flex-1 p-10 text-2xl font-bold">
+        Content goes here
+      </div>
     </div>
   );
 }
