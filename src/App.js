7fa955845f44898e4eda7ee8cf2f49b2e2b97d5c
andiliewantorosusanto <andiliesusanto@gmail.com>
2022-03-28 18:13:04 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,42 +1,33 @@
-import './App.css';
-import React, { useRef, useState } from 'react';
-import DesktopNav from './DesktopNav';
-import MobileNav from './MobileNav';
-import TodoList from './TodoList';
+import React from 'react';
+import {
+  Route,
+  BrowserRouter,
+  Routes
+} from "react-router-dom";
+
+import DesktopNav from './components/desktopnav/DesktopNav';
+import MobileNav from './components/mobilenav/MobileNav';
+import Category from './pages/category/Category';
+import Home from './pages/home/Home';
 
 function App() {
-  const [todos] = useState([{id: 1, name: 'Todo 1', complete: false}]);
-  const todoNameRef = useRef();
-
-  function setTodos(todos) {
-    this.setState({ todos });
-  }
-
-  function handleAddTodo(e) {
-    e.preventDefault();
-    const newTodo = {
-      id: todos.length + 1,
-      name: todoNameRef.current.value,
-      complete: false
-    };
-    setTodos([...todos, newTodo]);
-    todoNameRef.current.value = '';
-  }
 
   return (
-    <div className="relative min-h-screen md:flex">
-      <MobileNav />
-      <DesktopNav />
+    <BrowserRouter>
+      <div className="relative min-h-screen md:flex">
+        <MobileNav />
+        <DesktopNav />
 
-      <div className="flex-1 p-10 text-2xl font-bold">
-        <input type="text" className="w-full p-2" placeholder="Add a todo" />
-        <input type="submit" className="w-full p-2" value="Add" onClick={handleAddTodo}/>
-        <TodoList todos={todos} />
-        <div>
-          {todos.length} Left To Do
+        <div className="flex-1 p-10 text-2xl font-bold">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/category" element={<Category />} />
+          </Routes>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
+
+
   );
 }
 
